82df7cd4596bb7fef059114a951d11025b4f3988
Alejandro <xespitia@hotmail.com>
2018-12-02 22:09:35 -0500

    Update home.ts

--- Ionic-Commerline/src/pages/home/home.ts
+++ Ionic-Commerline/src/pages/home/home.ts
@@ -1,8 +1,12 @@
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { EditPage } from '../edit/edit';
+import { LoadingController } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { HttpClient } from '@angular/common/http';
+import { AgregarProductoPage } from '../agregar-producto/agregar-producto';
 
-
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,85 +14,156 @@ import { HttpClient } from '@angular/common/http';
 export class HomePage {
 
 
-   /**
-    * @name items
-    * @type {Array}
-    * @public
-    * @description     Used to store returned PHP data
-    */
-   public items : Array<any> = [];
+  posts: any;
+  items: any;
+  name: any;
+  image_name: any;
+  price: any;
+  description: any;
+  p: number = 1;
+
+  constructor(public navCtrl: NavController, private http: Http, public loading: LoadingController, public navParams: NavParams,
+    public alertCtrl: AlertController) {
+
+    let loader = this.loading.create({
+      content: 'Cargando productos…',
+    });
+
+    loader.present().then(() => {
+      this.http.get('http://localhost/ionic-php-mysql/retrieve-data.php').map(res => res.json()).subscribe(
+        data => {
+
+          let headers = new Headers();
+          headers.append('Content-Type', 'application/json');
+
+          this.posts = data.productos;
+          this.initializeItems();
+          loader.dismiss()
+
+        },
+        error => {
+          console.log("Oops!", error);
+        }
+
+      );
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ViewProductsPage');
+  }
+
+  Post() {
+    this.navCtrl.push(AgregarProductoPage)
+  }
+
+  editPost(item) {
+    this.navCtrl.push(EditPage, item)
+
+  }
+
+  initializeItems() {
+    this.items = this.posts;
+    this.p = 1;
+  }
+
+  getItems(ev: any) {
+    // Restablecer artículos de nuevo a todos los artículos
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // si el valor es una cadena vacía no filtre los elementos
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  deletePost(item) {
+
+    let alert = this.alertCtrl.create({
+
+      title: 'Confirmar eliminación',
+      message: 'Estas seguro de eliminar este producto?',
+      buttons: [
+
+        {
+
+          text: 'Cancelar',
+          role: 'Cancelar',
+
+          handler: () => {
+
+            console.log('Cancelar seleccionado');
+
+          }
+
+        },
+
+        {
+
+          text: 'Eliminar',
+
+          handler: () => {
+
+            var headers = new Headers();
+            headers.append("Accept", 'application/json');
+            headers.append('Content-Type', 'application/json');
+            let options = new RequestOptions({ headers: headers });
 
 
+            let loader = this.loading.create({
+              content: 'Procesando por favor espere…',
+            });
 
-   constructor(public navCtrl: NavController,
-               public http   : HttpClient)
-   {
+            loader.present().then(() => {
+              this.http.post('http://localhost/ionic-php-mysql/delete_data.php', item, options)
+                .map(res => res.json())
+                .subscribe(res => {
 
-   }
+                  loader.dismiss()
 
-   /**
-    * Triggered when template view is about to be entered
-    * Returns and parses the PHP data through the load() method
-    *
-    * @public
-    * @method ionViewWillEnter
-    * @return {None}
-    */
-   ionViewWillEnter() : void
-   {
-      this.load();
-   }
+                  if (res == "datos eliminados exitosamente") {
 
-   /**
-    * Retrieve the JSON encoded data from the remote server
-    * Using Angular's Http class and an Observable - then
-    * assign this to the items array for rendering to the HTML template
-    *
-    * @public
-    * @method load
-    * @return {None}
-    */
-   load() : void
-   {
-      this.http
-      .get('http://localhost/ionic-php-mysql/retrieve-data.php')
-      .subscribe((data : any) =>
-      {
-         console.dir(data);
-         this.items = data;
-      },
-      (error : any) =>
-      {
-         console.dir(error);
-      });
-   }
+                    let alert = this.alertCtrl.create({
 
-   /**
-    * Allow navigation to the AgregarProductoPage for creating a new entry
-    *
-    * @public
-    * @method addEntry
-    * @return {None}
-    */
-   addEntry() : void
-   {
-      this.navCtrl.push('AgregarProductoPage');
-   }
+                      title: "HECHO",
+                      subTitle: (res),
+                      buttons: ['OK']
+                    });
 
-   /**
-    * Allow navigation to the AgregarProductoPage for amending an existing entry
-    * (We supply the actual record to be amended, as this method's parameter,
-    * to the AgregarProductoPage
-    *
-    * @public
-    * @method viewEntry
-    * @param param 		{any} 			Navigation data to send to the next page
-    * @return {None}
-    */
-   viewEntry(param : any) : void
-   {
-      this.navCtrl.push('AgregarProductoPage', param);
-   }
+                    alert.present()
+                    this.navCtrl.push(HomePage);
+                  }
+                  else {
 
+                    let alert = this.alertCtrl.create({
+
+                      title: "ERROR",
+                      subTitle: (res),
+                      buttons: ['OK']
+                    });
+
+                    alert.present();
+                  }
+
+                });
+
+            });
+
+          }
+
+        }
+
+      ]
+
+    });
+
+    alert.present();
+
+  }
 
 }
