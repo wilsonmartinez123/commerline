8b64d023b592912327b145abc3a73809875dd5f3
Alejandro <xespitia@hotmail.com>
2019-01-19 16:42:29 -0500

    Add files via upload

--- Ionic-Commerline/src/pages/account/account.ts
+++ Ionic-Commerline/src/pages/account/account.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { AlertController, NavController, ModalController } from 'ionic-angular';
 
 import { UserData } from '../../providers/user-data';
+import { ChangePasswordPage } from '../change-password/change-password';
+import { AgregarEmpresaPage } from '../agregar-empresa/agregar-empresa';
+import { Http, Headers } from '@angular/http';
 //import { Item } from '../../assets/item';
 //import { Items } from '../../providers';
 
@@ -13,13 +16,33 @@ import { UserData } from '../../providers/user-data';
 })
 export class AccountPage {
   username: string;
+  users: any;
+
   //currentItems: Item[];
 
   constructor(public alertCtrl: AlertController, public nav: NavController, public userData: UserData,
-    public modalCtrl: ModalController,
+    public modalCtrl: ModalController,  private http: Http
     //public items: Items
   ) {
-   // this.currentItems = this.items.query();
+    // this.currentItems = this.items.query();
+
+
+    //obtener usuarios
+    this.http.get('http://localhost/ionic-php-mysql/obtener_usuarios.php').map(res => res.json()).subscribe(
+      data => {
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.users = data.usuarios;
+        //this.categories = Array.of(this.categories);
+
+      },
+      error => {
+        console.log("Oops!", error);
+      }
+
+    );
   }
 
   ngAfterViewInit() {
@@ -62,8 +85,10 @@ export class AccountPage {
     });
   }
 
-  changePassword() {
+  changePassword(item) {
     console.log('Clicked to change password');
+    //this.nav.push('ChangePasswordPage');
+    this.nav.push(ChangePasswordPage, item)
   }
 
   logout() {
@@ -77,7 +102,15 @@ export class AccountPage {
 
 
   agregarProducto() {
+    this.nav.push('AgregarProductoPage');
+  }
+
+  verProducto() {
     this.nav.push('HomePage');
+  }
+
+  agregarEmpresa() {
+    this.nav.push(AgregarEmpresaPage)
   }
 
 }
